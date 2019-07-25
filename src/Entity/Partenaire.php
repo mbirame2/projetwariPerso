fcd1105f18146e6557fd836d0795e402da618214
mbirame2 <mbirame2000@gmail.com>
2019-07-25 11:48:55 +0000

    ajout classe versement

--- src/Entity/Partenaire.php
+++ src/Entity/Partenaire.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -32,6 +34,26 @@ class Partenaire
      * @ORM\Column(type="string", length=255)
      */
     private $adresse;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $numeroCompte;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $solde;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Versement", mappedBy="partenaire")
+     */
+    private $versements;
+
+    public function __construct()
+    {
+        $this->versements = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -70,6 +92,61 @@ class Partenaire
     public function setAdresse(string $Adresse): self
     {
         $this->adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getNumeroCompte(): ?int
+    {
+        return $this->numeroCompte;
+    }
+
+    public function setNumeroCompte(int $numeroCompte): self
+    {
+        $this->numeroCompte = $numeroCompte;
+
+        return $this;
+    }
+
+    public function getSolde(): ?int
+    {
+        return $this->solde;
+    }
+
+    public function setSolde(int $solde): self
+    {
+        $this->solde = $solde;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Versement[]
+     */
+    public function getVersements(): Collection
+    {
+        return $this->versements;
+    }
+
+    public function addVersement(Versement $versement): self
+    {
+        if (!$this->versements->contains($versement)) {
+            $this->versements[] = $versement;
+            $versement->setPartenaire($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVersement(Versement $versement): self
+    {
+        if ($this->versements->contains($versement)) {
+            $this->versements->removeElement($versement);
+            // set the owning side to null (unless already changed)
+            if ($versement->getPartenaire() === $this) {
+                $versement->setPartenaire(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Versement.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\VersementRepository")
+ */
+class Versement
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $montant;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Partenaire", inversedBy="versements")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $partenaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+
+    public function getPartenaire(): ?Partenaire
+    {
+        return $this->partenaire;
+    }
+
+    public function setPartenaire(?Partenaire $partenaire): self
+    {
+        $this->partenaire = $partenaire;
+
+        return $this;
+    }
+}
